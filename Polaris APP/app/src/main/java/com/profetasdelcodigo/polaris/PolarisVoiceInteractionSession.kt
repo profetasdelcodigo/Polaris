fd452af9bc3a999799acba8fe73fc0cb8c3e46d5
profetasdelcodigo <profetas.del.codigo@gmail.com>
2026-09-20 08:19:13 -0500

    fix(android): import Ktor request body extension

--- Polaris APP/app/src/main/java/com/profetasdelcodigo/polaris/PolarisVoiceInteractionSession.kt
+++ Polaris APP/app/src/main/java/com/profetasdelcodigo/polaris/PolarisVoiceInteractionSession.kt
@@ -26,6 +26,7 @@ import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.header
 import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.contentType

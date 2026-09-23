5e7d17e7d110206b3dce7e1c4b16383fbd8852f0
profetasdelcodigo <profetas.del.codigo@gmail.com>
2026-09-23 13:47:58 -0500

    fix(android): compile API 37 while targeting Android 16

--- Polaris APP/app/build.gradle.kts
+++ Polaris APP/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.profetasdelcodigo.polaris"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.profetasdelcodigo.polaris"
